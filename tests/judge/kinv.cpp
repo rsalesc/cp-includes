650b965db643c1bbe458f81aa17daa131f5b6c10
Roberto <iseme.beto@gmail.com>
2018-10-16 21:35:19 -0300

    Add 4-dft mod_split_fft

--- tests/judge/kinv.cpp
+++ tests/judge/kinv.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
-#include "../../PolynomialRing.cpp"
+#include "../../ModularInteger.cpp"
 #include "../../LongMultiplication.cpp"
 
 using namespace std;
 using namespace lib;
 
 typedef long long ll;
+
+const int MOD = (int)1e9+7;
+using Field = Mint32<MOD>;
 
 int32_t main() {
   ios::sync_with_stdio(false); cin.tie(0);
@@ -14,12 +17,12 @@ int32_t main() {
 
   int n = s.size();
 
-  vector<ll> a(n), b(n);
+  vector<Field> a(n), b(n);
   for(int i = 0; i < n; i++) {
     if(s[i] == 'A') a[i] = 1;
     else b[i] = 1;
   }
 
-  auto res = math::circular_conv<math::FastMultiplication>(a, b);
+  auto res = math::circular_conv<math::SafeMultiplication>(a, b);
   for(int i = 1; i < n; i++) cout << res[i] << endl;
 }
