5113d281ff3ef2ccb36ca9e3429ca4a36750851c
Roberto <iseme.beto@gmail.com>
2020-11-14 02:40:21 -0300

    Improve PowerSeries exp.

--- PowerSeries.cpp
+++ PowerSeries.cpp
@@ -7,42 +7,32 @@
 namespace lib {
 using namespace std;
 namespace series {
-
+  
 template <typename P> P ln(const P &p, int n);
 
 template <typename P> P inverse(P p, int n) {
   return p.inverse(n);
 }
 
-namespace two { // functions for n = 2^k, k >= 0
-
-// p[0] must be invertible
-// if p[0] is 1, correctly computes ln(p)
-// else computes weirdly scaled ln(p)
 template <typename P> P ln(const P &p, int n) {
   return (p.derivative() * inverse(p, n) % n).integral() % n;
 }
 
+template<typename P> pair<P, P> exp2(P p, int n) {
+  assert(p[0] == 0);
+  P f{1}, g{1};
+  for(int i = 1; i < n; i*=2) {
+    g = g * 2 - (g*g%i*f)%i;
+    P q = (p % i).derivative();
+    q += g * (f.derivative() - f * q) % (2 * i - 1);
+    f += f * (p % (2 * i) - q.integral()) % (2 * i);
+  }
+  return {f % n, g % n};
+}
+
 // p[0] must be null
 template <typename P> P exp(P p, int n) {
-  if (n == 1)
-    return P(1);
-  p %= n;
-  P q = exp(p, n >> 1);
-  return q * (p - ln(q, n) + P(1)) % n;
-}
-} // namespace two
-
-template <typename P> P ln(const P &p, int n) {
-  if (n <= 0)
-    return P();
-  return two::ln(p, next_power_of_two(n)) % n;
-}
-
-template <typename P> P exp(const P &p, int n) {
-  if (n <= 0)
-    return P();
-  return two::exp(p, next_power_of_two(n)) % n;
+  return exp2(p, n).first;
 }
 
 template <typename P> P power(const P &p, long long k, int n) {
