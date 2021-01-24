39ca492f1d470b502a9a6e2384b82f6b2533901a
Roberto Sales <iseme.beto@gmail.com>
2021-01-24 03:50:18 -0300

    Fix Github Actions exit status.

--- scripts/run_stress.sh
+++ scripts/run_stress.sh
@@ -4,5 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR/../tests/stress
 mkdir -p build && pushd build
 cmake .. && cmake --build . && ./Lib_Stress
+status=$?
 popd
 popd
+
+exit $status
