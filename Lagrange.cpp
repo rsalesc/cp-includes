80a8dfd6624ca0aa92f8d35d86224f839c418b32
Roberto <iseme.beto@gmail.com>
2021-02-12 00:04:03 -0300

    Improve and fix Lagrange iota interpolation.

--- Lagrange.cpp
+++ Lagrange.cpp
@@ -1,33 +1,24 @@
 #ifndef _LIB_LAGRANGE
 #define _LIB_LAGRANGE
 #include <bits/stdc++.h>
+#include "Combinatorics.cpp"
 
 namespace lib {
 using namespace std;
 namespace linalg {
 template <typename Field> struct PrefixLagrange {
-  vector<Field> fat, ifat, pref, suf;
-  PrefixLagrange() {
-    fat = {1};
-    ifat = {1};
-  }
+  vector<Field> pref, suf;
+  PrefixLagrange() {}
 
   void ensure(int n) {
-    int o = fat.size();
-    if (n + 1 <= o)
+    int o = pref.size();
+    if (n <= o)
       return;
-    fat.resize(n + 1), ifat.resize(n + 1);
-    pref.resize(n + 5), suf.resize(n + 5);
-
-    for (int i = o; i <= n; i++)
-      fat[i] = fat[i - 1] * i;
-
-    ifat.back() = Field(1) / fat.back();
-    for (int i = n - 1; i >= o; i--)
-      ifat[i] = ifat[i + 1] * (i + 1);
+    pref.resize(n), suf.resize(n);
   }
 
   template <typename T> Field eval(const vector<Field> &v, T x) {
+    using C = Combinatorics<Field>;
     assert(!v.empty());
     int d = (int)v.size() - 1;
     if (x <= d)
@@ -35,22 +26,38 @@ template <typename Field> struct PrefixLagrange {
 
     ensure(d + 1);
 
+    Field a = x;
     pref[0] = suf[d] = 1;
-    for (int i = 0; i < d; i++)
-      pref[i + 1] = pref[i] * (x - i);
-    for (int i = d; i; i--)
-      suf[i - 1] = suf[i] * (x - i);
+    for (T i = 0; i < d; i++)
+      pref[i + 1] = pref[i] * a, a -= 1;
+    for (T i = d; i; i--)
+      suf[i - 1] = suf[i] * a, a += 1;
 
     Field ans = 0;
     for (int i = 0; i <= d; i++) {
-      Field l = pref[i] * suf[i] * ifat[i] * ifat[d - i];
+      Field l = pref[i] * suf[i] * C::ifactorial(i) * C::ifactorial(d-i) * v[i];
       if ((d + i) & 1)
         l = -l;
-      ans += l * v[i];
+      ans += l;
     }
     return ans;
   }
 };
+
+template<typename T, typename U>
+T lagrange_iota(const vector<T>& f, U n) {
+  static PrefixLagrange<T> lag;
+  return lag.eval(f, n);
+}
+
+template<typename T, typename U>
+T lagrange_iota_sum(const vector<T>& f, U n) {
+  int m = f.size();
+  vector<T> g(m + 1);
+  for(int i = 1; i <= m; i++)
+      g[i] = g[i-1] + f[i-1];
+  return lagrange_iota(g, n);
+}
 } // namespace linalg
 } // namespace lib
 
