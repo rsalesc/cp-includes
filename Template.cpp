fc7abec8b9990823b41036369239cc78d871f313
Roberto Sales <rsalesc@google.com>
2023-02-27 10:03:35 -0300

    A few fixes in corp.

--- Template.cpp
+++ Template.cpp
@@ -11,9 +11,22 @@ using namespace std;
 #define ss second
 #define iopt ios::sync_with_stdio(false); cin.tie(0)
 #define untie(p, a, b) decltype(p.first) a = p.first, decltype(p.second) b = p.second
+#define TESTCASE(tn) cout << "Case #" << tn << ": "
  
 int gcd(int a, int b) { return b == 0 ? a : gcd(b, a%b); }
+
+int floor2(int x, int y);
+int ceil2(int x, int y) {
+  if(y < 0) return ceil2(-x, -y);
+  return x < 0 ? -floor2(-x, y) : (x + y - 1) / y;
+}
+int floor2(int x, int y) {
+  if(y < 0) return floor2(-x, -y);
+  return x < 0 ? -ceil2(-x, y) : x / y;
+}
  
 typedef pair<int, int> ii;
 typedef long double LD;
 typedef vector<int> vi;
+
+#define TC_MAIN int32_t main() { iopt; int T; cin >> T; for(int i = 1; i <= T; i++) solve(i); }
--- bits/stdc++.h
+++ bits/stdc++.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cerrno>
 #include <cfloat>
 #include <ciso646>
