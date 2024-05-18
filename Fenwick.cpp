b078cbaf2fc7df00e0e5b9810213ddf821351cf9
Roberto <rsalescaldeira@gmail.com>
2024-05-18 09:53:05 -0300

    Make Fenwick binary searchable.

--- Fenwick.cpp
+++ Fenwick.cpp
@@ -6,10 +6,10 @@ namespace lib {
 using namespace std;
 template<typename T>
 struct Fenwick {
-  vector<int> t;
+  vector<T> t;
   Fenwick(int n) : t(n+1) {}
-  int size() const { return t.size() - 1; }
-  void add(int i, T x) {
+  int size() const { return(int) t.size() - 1; }
+  void add(int i, const T& x) {
     for(i++; i < t.size(); i += (i&-i))
       t[i] += x;
   }
@@ -24,6 +24,28 @@ struct Fenwick {
   }
   T from(int i) const {
     return get(i, size() - 1);
+  }
+  template<typename Func>
+  int find_last(const Func& func) {
+    int n = size();
+    int msb = 32 - __builtin_clz(n);
+    int res = 0;
+    T acc = 0;
+    for(int b = msb; b >= 0; b--) {
+      T nxt = acc + t[res | (1 << b)];
+      if (func(nxt)) {
+        acc = nxt;
+        res |= (1 << b);
+      }
+    }
+    return res-1;
+  }
+
+  template<typename Func>
+  int find_first(const Func& func) {
+    return find_last([&](const T& val) {
+      return !func(val);
+    }) + 1;
   }
 };
 } // namespace lib
